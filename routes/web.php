08cba229d10821e892b9c2bb7aeacaf140d81567
ankasha77 <yudhaxd1@gmail.com>
2026-06-06 15:49:22 +0700

    tambah AboutController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AboutController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,3 +13,5 @@ Route::get('/', function () {
 Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/contact', [ContactController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'index']);
