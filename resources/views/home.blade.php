809300b4e7ca376391a4d3f644b43c619bbfe029
ankasha77 <yudhaxd1@gmail.com>
2026-06-06 15:03:35 +0700

    ubah about dan contact

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,8 +39,8 @@
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link active" href="home.html">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact">Contact</a></li>
             </ul>
         </div>
     </div>
